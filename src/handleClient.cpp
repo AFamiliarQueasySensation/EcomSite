77303b00087204ff6864b580b722d01f68e3f635
Grant Andrew Von Hagen <vonhagengrant@gmail.com>
2025-02-13 01:55:22 -0500

    Added parsing for headers, and content for response body

--- src/handleClient.cpp
+++ src/handleClient.cpp
@@ -1,38 +1,131 @@
 #include "../src/handleClient.h"
 
 #include <QTcpSocket>
+#include <QRegularExpression>
 #include <QDebug>
 
 handleClient::handleClient(QTcpSocket *socket, QObject *parent) : QObject(parent), m_socket(socket)
 {
     connect(m_socket.get(), &QAbstractSocket::disconnected, this, &QObject::deleteLater);
     connect(m_socket.get(), &QIODevice::readyRead, this, &handleClient::readReady);
-    
 }
 
+handleClient::~handleClient() {}
 
 void handleClient::readReady()
 {
-    while(m_socket->canReadLine()){
+    while ((m_state == ConnectionState::RequestLine || m_state == ConnectionState::RequestHeaders) && m_socket.get()->canReadLine())
+    {
 
-        const auto line = m_socket->readLine();
-        if (line == "\r\n"){
-
-            m_socket->write("HTTP/1.1 200 OK \r\n");
-            m_socket->write("Connection: keep-alive\r\n");
-            m_socket->write("Content-Length: 10\r\n");
-            m_socket->write("\r\n"); //end header
-            m_socket->write("Fuck yo bitch ass ");
-            m_socket->flush();
-
+        auto line = m_socket->readLine();
+        if (line.endsWith("\r\n"))
+        {
+            line.chop(2);
         }
 
+        if (m_state == ConnectionState::RequestLine)
+        {
 
+            const auto parts = line.split(' ');
 
+            if (parts.size() != 3)
+            {
+                qDebug() << "Incorrect Request Line Size Passed" << line;
+                deleteLater();
+                return;
+            }
+            // handle parts, protocol, and path
+            m_request.method = parts.at(0);
+            m_request.path = parts.at(1);
+            m_request.protocol = parts.at(2);
 
+            m_state = ConnectionState::RequestHeaders;
+
+            continue;
+        }
+        else
+        {
+            if (line.isEmpty())
+            {
+                if (m_request.contentLength)
+                {
+                    m_state = ConnectionState::RequestBody;
+                    continue;
+                }
+                else
+                {
+                    m_state = ConnectionState::Response;
+                    sendResponse();
+                    return;
+                }
+            }
+
+            static const QRegularExpression expr("^([^:]+): +(.*)$");
+            const auto match = expr.match(line);
+            if (!match.hasMatch())
+            {
+                qDebug() << "No match found in handleClient.cpp";
+                deleteLater();
+                return;
+            }
+
+            const auto name = match.captured(1);
+            const auto value = match.captured(2);
+
+            m_request.headers[name] = value;
+
+            if (name == "Content-Length")
+            {
+                bool ok;
+                m_request.contentLength = value.toInt(&ok);
+
+                if (!ok)
+                {
+                    qDebug() << "Could not parse Content-Length in handleClient.cpp: " << line;
+                    deleteLater();
+                    return;
+                }
+            }
+
+            continue;
+        }
+    }
+
+    // Parse body
+    if (m_state == ConnectionState::RequestBody)
+    {
+
+        m_request.body.append(m_socket->read(m_request.contentLength - m_request.body.size()));
+
+        if (m_request.body.size() == m_request.contentLength)
+        {
+
+            m_state = ConnectionState::Response;
+            sendResponse();
+            return;
+        }
     }
 }
 
 
 
-handleClient::~handleClient(){}
+
+void handleClient::sendResponse()
+{
+    QString content;
+    content += "testing";
+    
+
+
+    const auto encode = content.toUtf8();
+
+    m_socket->write("HTTP/1.1 200 OK\r\n");
+    m_socket->write("Content-Type: text/html\r\n");
+    m_socket->write(QString("content-Length: %0\r\n").arg(encode.size()).toUtf8());
+    m_socket->write("\r\n");
+    m_socket->write(encode);
+   
+
+    m_state = ConnectionState::RequestLine;
+    m_request = {};
+}
--- src/handleClient.h
+++ src/handleClient.h
@@ -1,25 +1,46 @@
 #pragma once
 
 #include <QObject>
+#include <QMap>
+#include <QByteArray>
 
 class QTcpSocket;
 
-
-class handleClient: public QObject{
+class handleClient : public QObject
+{
 
     Q_OBJECT
 
-    public:
-        explicit handleClient(QTcpSocket *socket, QObject *parent = nullptr);
-        ~handleClient() override;
+public:
+    explicit handleClient(QTcpSocket *socket, QObject *parent = nullptr);
+    ~handleClient() override;
 
-    private slots:
-        void readReady();
+private slots:
+    void readReady();
 
+private:
+    void sendResponse();
+    const std::unique_ptr<QTcpSocket> m_socket;
 
-    private:
-        const std::unique_ptr<QTcpSocket> m_socket;
+    struct Request {
+        std::size_t contentLength{};
 
+        QString method;
+        QString path;
+        QString protocol;
 
+        QMap<QString, QString> headers;
+        QByteArray body;
+    };
+
+    Request m_request;
+
+    enum ConnectionState {
+        RequestLine,
+        RequestHeaders,
+        RequestBody,
+        Response
+    };
+
+    ConnectionState m_state{ConnectionState::RequestLine};
 };
-
